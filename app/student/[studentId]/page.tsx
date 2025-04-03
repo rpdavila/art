58e2d52f9b3d7fbc69cede90946aa276d15f8e24
rpdavila <rafael.pietri@gmail.com>
2025-04-03 07:02:26 +0200

    removed map funstion in component

--- app/student/[studentId]/page.tsx
+++ app/student/[studentId]/page.tsx
@@ -2,19 +2,17 @@ import { data } from "@/app/data/data";
 import Image from "next/image";
 export default async function Student({ params }: { params: Promise<{ studentId: string }> }) {
   const { studentId } = await params;
-  const students = data.find((student) => student.students)
+  const students = data.find((student) => student.students.some((student) => student.studentId === studentId));
+  const student = students?.students.find((student) => student.studentId === studentId);
+  if (!student) {
+    return <div>Student not found</div>
+  }
   return (
-
     <section className="flex flex-col items-center min-h-screen sm:content-center">
-      {students?.students.map(student => {
-        if (student.studentId !== studentId) return null
-        return (
-          <div key={student.studentId} className="flex flex-col justify-center items-center">
-            <Image className="rounded-lg" src={student.artPiece} alt="student art" width={500} height={500} />
-            <h1 className="text-2xl font-bold">{student.name}</h1>
-          </div>
-        )
-      })}
+      <div key={student.studentId} className="flex flex-col justify-center items-center min-h-screen">
+        <Image className="rounded-lg" src={student.artPiece} alt="student art" width={500} height={500} />
+        <h1 className="text-2xl font-bold">{student.name}</h1>
+      </div>
     </section>
   )
 }
